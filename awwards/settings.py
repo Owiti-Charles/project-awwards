ad988b33f216893bb1867ddc2691c02961e91450
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-20 14:34:01 +0300

    Display user details on their profile page

--- awwards/settings.py
+++ awwards/settings.py
@@ -115,8 +115,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'index'
 
