66be1db181f7ce17a4661a23c730ac792ef23cd5
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-20 13:37:50 +0300

    Edit navbar to display username on the logged in user

--- awwards/settings.py
+++ awwards/settings.py
@@ -118,7 +118,6 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 LOGIN_REDIRECT_URL = 'index'
 
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'index'
