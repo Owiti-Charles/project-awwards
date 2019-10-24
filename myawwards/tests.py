0439eaef97c4e0d3f55164e503c66f32e67e3e39
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-24 11:40:54 +0300

    Update background image and add more styling

--- myawwards/tests.py
+++ myawwards/tests.py
@@ -10,6 +10,12 @@ class TestProfile(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.user, User))
 
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
     def tearDown(self):
         self.user.delete()
 
